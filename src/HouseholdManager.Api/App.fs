9d96dea3b2b03a2c48110f74d43c6e24d335f924
Jiri Landsman <landsman@cngroup.dk>
2017-01-18 23:00:03 +0100

    ports

--- src/HouseholdManager.Api/App.fs
+++ src/HouseholdManager.Api/App.fs
@@ -1,8 +1,14 @@
 ﻿open Suave
 open Suave.Successful
+open System.Net
+open System
 
 [<EntryPoint>]
-let main argv = 
+let main [| port |] = 
 
-    startWebServer defaultConfig (OK "Household Manager Api")
+    let config =
+        { defaultConfig with
+            bindings = [ HttpBinding.create HTTP IPAddress.Loopback (uint16 port) ]
+            listenTimeout = TimeSpan.FromMilliseconds 3000. }
+    startWebServer config (OK "Household Manager Api")
     0
